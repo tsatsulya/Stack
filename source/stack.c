c58e626b748a36db7294f748a7c903ee27f1f9f4
tsatsulya <dmitrytsatsulin@gmail.com>
2022-10-15 02:16:58 +0300

    need to fix canaries

--- source/stack.c
+++ source/stack.c
@@ -7,6 +7,8 @@
 static Canary_data CANARY = 0x000DEAD000;
 
 
+
+
 static size_t stack_get_buffer_size(size_t length) {
 
     size_t size = length*sizeof(Stack_data) + 2*sizeof(Canary_data);
@@ -18,6 +20,7 @@ static char* stack_get_data_base_ptr(Stack* stack) {
 
      return stack->data + sizeof(Canary_data);
 }  
+
 
 
 
@@ -33,6 +36,7 @@ static Stack_data get_element(Stack* stack, long long int position) {
     
     return *get_ptr_to_element(stack, position);
 } 
+
 
 
 static fullness get_stack_occupancy_status(Stack* stack) {
@@ -60,6 +64,7 @@ void print_size_info(Stack* stack) {
 }
 
 
+
 static stack_status stack_length_up(Stack* stack) {
     stack_change_length(stack, 2);
     //
@@ -72,10 +77,7 @@ static stack_status stack_length_down(Stack* stack) {
     return 0;
 }
 
-static void get_canaries_positions(Stack* stack, Canary_data** left_canary, Canary_data** right_canary) { //in structure???
-    *left_canary = (Canary_data*)stack->data;
-    *right_canary = (Canary_data*)(stack->data + sizeof(Stack_data) * stack->capacity + sizeof(Canary_data));
-}
+
 
 static Canary_data* get_left_canary_position(Stack* stack) {
     return (Canary_data*)stack->data;
@@ -85,10 +87,19 @@ static Canary_data* get_right_canary_position(Stack* stack) {
     return (Canary_data*)(stack->data + sizeof(Stack_data) * stack->capacity + sizeof(Canary_data));
 }
 
+
+//что-то с канарейками я рот их ебала давайте работайте блять твари
+static void print_canaries(Stack* stack) {
+
+    printf("left_canary: %llx\n", *stack->left_canary_position);
+    printf("right_canary: %llx\n", *stack->right_canary_position);
+}
 static stack_status set_canaries(Stack* stack){
+    if (0) {
 
     *stack->left_canary_position = *stack->right_canary_position = CANARY;
-
+    print_canaries(stack);
+    }
     return SUCCESS;
 }
 
@@ -99,11 +110,7 @@ static stack_status set_canaries(Stack* stack){
 //     get_canaries_positions(stack, &left_canary,  &right_canary);
 // }) :((((((((((((((((((
 
-static void print_canaries(Stack* stack) {
 
-    printf("left_canary: %llx\n", *stack->left_canary_position);
-    printf("right_canary: %llx\n", *stack->right_canary_position);
-}
 
 static stack_status stack_check_canaries(Stack* stack) {
 
@@ -114,6 +121,47 @@ static stack_status stack_check_canaries(Stack* stack) {
 
     return status;
 }
+
+
+
+static unsigned long int gnu_hash(void* data, size_t size) {
+
+    char* hash_data = (char*) data;
+    unsigned long int h = 0;
+    
+    for (unsigned long int c = 0; c < size; c++) {    
+        h = h * 33  + *(hash_data+c);
+    }
+
+    return 0;
+}
+
+static unsigned long int stack_hash(Stack* stack) {
+
+    int data_hash = gnu_hash(stack_get_data_base_ptr(stack), stack->capacity * sizeof(Stack_data));
+    int struct_hash = gnu_hash(stack, sizeof(Stack));
+
+    return data_hash ^ struct_hash;
+}
+
+static int stack_rehash(Stack* stack) {
+
+    stack->hash = stack_hash(stack);
+
+    return SUCCESS;
+}
+
+static int stack_check_hash(Stack* stack) {
+
+    if (!stack)
+        return STACK_NULL;
+
+    unsigned long int correct_stack_hash = stack->hash;
+    unsigned long int actual_stack_hash = stack_hash(stack);
+
+    return (actual_stack_hash == correct_stack_hash) ? SUCCESS : INVALID_HASH;
+}
+
 
 void stack_data_print(Stack* stack) {
 
@@ -139,7 +187,6 @@ stack_status stack_init(Stack* stack, size_t length) {
     };
 
    
-
     printf("___init_stack: %p\n", stack);
     printf("___init_data: %p\n", stack->data);
     
@@ -147,11 +194,15 @@ stack_status stack_init(Stack* stack, size_t length) {
     set_canaries(stack);
     stack->left_canary_position = get_left_canary_position(stack);
     stack->right_canary_position = get_right_canary_position(stack);
+
+
+    stack_rehash(stack);
     //print_canaries(stack);
     // hash + canaries
     //(stack)
     return 0;
 }
+
 
 stack_status stack_push(Stack* stack, Stack_data element) {
 
@@ -160,12 +211,16 @@ stack_status stack_push(Stack* stack, Stack_data element) {
     *get_ptr_to_element(stack, stack->element_count++) = element;
 
     //stack_data_print(stack);
+    stack_rehash(stack);
+
     return 0;
 }
 
 Stack_data stack_pop(Stack* stack) {
 
     Stack_data last_element = get_element(stack, stack->element_count--);
+
+    stack_rehash(stack);
 
     return last_element;
 }
@@ -182,9 +237,12 @@ stack_status stack_resize(Stack* stack) {
         puts("length down!");
         stack_length_down(stack);
     }
+
     set_canaries(stack);
     stack->left_canary_position = get_left_canary_position(stack);
     stack->right_canary_position = get_right_canary_position(stack);
+    stack_rehash(stack);
+
     return 0;
 }
 
